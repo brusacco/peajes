b06514456481129e9d008f69bc6f3c12f6380ae3
Bruno Sacco <brusacco@gmail.com>
2023-05-01 14:48:29 -0400

    aa

--- detect.py
+++ detect.py
@@ -1,15 +1,11 @@
 import jetson.inference
 import jetson.utils
-import cv2
 
 # initialize the camera
 camera = jetson.utils.gstCamera(1280, 720, "0")
 
 # load the object detection model
-net = jetson.inference.detectNet("yolov4-tiny", threshold=0.5)
-
-# create a window to display the output
-display = jetson.utils.glDisplay()
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 
 # process frames from the camera
 while True:
@@ -25,12 +21,15 @@ while True:
     # draw bounding boxes around detected objects
     for detection in detections:
         left, top, right, bottom = int(detection.Left), int(detection.Top), int(detection.Right), int(detection.Bottom)
-        print("Object: {:s} - Confidence: {:.2f}".format(net.GetClassDesc(detection.ClassID), detection.Confidence))
+        print("Object: {:s} - Confidence: {:.2f}".format(detection.ClassID, detection.Confidence))
         img = jetson.utils.cudaToNumpy(img, width, height, 4)
         img = cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
 
     # display the image
+    jetson.utils.cudaDeviceSynchronize()
+    jetson.utils.cudaDeviceSynchronize()
     display.RenderOnce(img, width, height)
+    display.SetTitle("Object Detection | {:d} FPS".format(int(1000.0 / net.GetNetworkTime())))
 
     # exit on ESC
     if display.IsClosed() or (device != None and device.IsStreaming() == False):
